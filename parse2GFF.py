ec742194d4ea26a3b2442e841b8321259fcf989d
Reza Taghavi <srtaghav@uark.edu>
2021-04-23 15:41:05 -0500

    update parse2gff

--- parse2GFF.py
+++ parse2GFF.py
@@ -3,6 +3,8 @@
 import csv
 import argparse
 from Bio import SeqIO
+from collections import defaultdict
+import re
 
 #inputs: 1) GFF file, 2) corresponding genome sequence (FASTA format)
 # create an argument parser object
@@ -17,12 +19,17 @@ args = parser.parse_args()
 
 # read in FASTA file
 genome = SeqIO.read(args.fasta, 'fasta')
-print(genome.id)
-print(genome.seq)
 
-(base, fileext) = input.split('.')
-outfile = base + 'Indexed' + '.fasta'
-out = open(outfile, 'w')
+print('Genome ID is, ', genome.id)
+print('Genome Sequence is, ', genome.seq)
+print('Genome length is, ', len(genome.seq))
+
+#create a nested dictionary
+CDSdict = defaultdict(dict)
+
+#(base, fileext) = input.split('.')
+#outfile = base + 'Indexed' + '.fasta'
+#out = open(outfile, 'w')
 
 # open and read in GFF file
 with open(args.gff, 'r') as gff_in:
@@ -31,18 +38,52 @@ with open(args.gff, 'r') as gff_in:
 
     # loop over all the lines in our reader object (i.e., parsed file)
     for line in reader: 
-        start = line[3]
-        end = line[4]
-        strand = line[6]
-        
-        # extract the sequence
-        out.write(len(genome.seq))
-        line = line.rstrip()
         if not line:
             continue
         else:
-            fields = line.split('\t')
-            out.write('\n')
-            out.write(str(fields[0] + ', ' + str(fields[8::])))
+            genus = line[0].split()[0]
+            species = line[0].split()[1]
+            features = line[2]
+            startcoords = str(int(line[3]) - 1)
+            endcoords = line[4]
+            strand = line[6]
+            separate = line[8].split(sep = ';')
+            line[-1] = separate
+            strconvert = str(separate)
+        if features != 'CDS':
+            continue
+        else:
+            cds = re.search('Gene\s(\S+)\s(\S+)?\s?(\d+)?', strconvert)
+            genename = cds.group(1)
+            exonnumber = cds.group(3)
 
-out.close()
+        CDScoords = [strand, startcoords, endcoords]
+        CDSdict[genename][exonnumber] = CDScoords
+        def sort(item: dict):
+            return {a: sort(b) if isinstance(b, dict) else b for a, b in sort(item.items())}
+
+CDSsortdict =  sort(CDSdict)
+CDSsortlist = list(CDSsortdict.items())
+
+for i in CDSsortlist:
+    gene = i[0]
+    genedictionary = i[1]
+    concatenate = ''
+
+    for x, y in genedictionary.items():
+        exonnumber = x
+        strand = y[0]
+        start = y[1]
+        end = y[2]
+
+        if strand == '+':
+            sequence = genome.seq[int(start):int(end)]
+            concatenate += sequence
+
+        else:
+            sequence = genome.seq[int(start):int(end)].reverse_complement()
+            concatenate += sequence
+
+print('>' + genus + ' ' + species + ' ' + gene)
+print(concatenate)
+print()
