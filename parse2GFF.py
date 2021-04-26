fef71052a543d9e4da1b1f872a1b7e7632a7ebc1
Reza Taghavi <srtaghav@uark.edu>
2021-04-26 05:26:13 -0500

    add outfile writing to parser

--- parse2GFF.py
+++ parse2GFF.py
@@ -12,24 +12,34 @@ parser = argparse.ArgumentParser(description = 'This script will parse a GFF fil
 
 # add positional arguments
 parser.add_argument("gff", help = 'name of the GFF file')
-parser.add_argument("fasta", help = 'name of the FASTA file')
+parser.add_argument("fasta", help = 'name of the FASTA/FSA file')
 
 # parse the arguments
 args = parser.parse_args()
 
 # read in FASTA file
-genome = SeqIO.read(args.fasta, 'fasta')
+genome = SeqIO.read(args.fasta.rstrip(), 'fasta')
 
-print('Genome ID is, ', genome.id)
-print('Genome Sequence is, ', genome.seq)
-print('Genome length is, ', len(genome.seq))
+outfile = 'gene_seq_x.fasta'
+out = open(outfile, 'w')
 
-#create a nested dictionary
+out.write('Genome ID is, ' + str(genome.id))
+out.write('\n')
+out.write('\n')
+out.write('Genome Sequence is, ' + str(genome.seq))
+out.write('\n')
+out.write('\n')
+out.write('Genome length is, ' + str(len(genome.seq)))
+out.write('\n')
+out.write('\n')
+
+def rev_comp(genome_seq, strand):
+    if strand == "-":
+        return(genome_seq.reverse_complement())
+    else:
+        return(genome_seq)
+        
 CDSdict = defaultdict(dict)
-
-#(base, fileext) = input.split('.')
-#outfile = base + 'Indexed' + '.fasta'
-#out = open(outfile, 'w')
 
 # open and read in GFF file
 with open(args.gff, 'r') as gff_in:
@@ -40,50 +50,35 @@ with open(args.gff, 'r') as gff_in:
     for line in reader: 
         if not line:
             continue
-        else:
-            genus = line[0].split()[0]
-            species = line[0].split()[1]
-            features = line[2]
-            startcoords = str(int(line[3]) - 1)
-            endcoords = line[4]
-            strand = line[6]
-            separate = line[8].split(sep = ';')
-            line[-1] = separate
-            strconvert = str(separate)
-        if features != 'CDS':
+        elif re.search('^#', line[0]):
             continue
         else:
-            cds = re.search('Gene\s(\S+)\s(\S+)?\s?(\d+)?', strconvert)
-            genename = cds.group(1)
-            exonnumber = cds.group(3)
+            start = line[3]
+            end = line[4]
+            strand = line[6]
+            species = line[0]
+            feature = line[2]
+            header = line[-1]
+            attributes = line[8]
+            exonnumber = re.search(r"exon\s(\d)", attributes)
+            new_header = ">" + species.replace(" ", "_") + "_" + attributes.split()[1]
 
-        CDScoords = [strand, startcoords, endcoords]
-        CDSdict[genename][exonnumber] = CDScoords
-        def sort(item: dict):
-            return {a: sort(b) if isinstance(b, dict) else b for a, b in sort(item.items())}
+            if feature == "CDS":
+                if(not exonnumber):
+                    CDSdict[new_header] = rev_comp(genome.seq[int(start)-1:int(end)], strand)
+                elif(not CDSdict[new_header]):
+                    CDSdict[new_header] = [' ']*256
+                    CDSdict[new_header][int(exonnumber[1])-1] = rev_comp(genome.seq[int(start)-1:int(end)], strand)
+                else:
+                    CDSdict[new_header][int(exonnumber[1])-1] = rev_comp(genome.seq[int(start)-1:int(end)], strand)
 
-CDSsortdict =  sort(CDSdict)
-CDSsortlist = list(CDSsortdict.items())
+for a in CDSdict:
+    CDSdict[a] = ''.join(str(exon) for exon in CDSdict[a])
 
-for i in CDSsortlist:
-    gene = i[0]
-    genedictionary = i[1]
-    concatenate = ''
+for a, b in CDSdict.items():
+    out.write(a)
+    out.write(b)
+    out.write('\n')
+    out.write('\n')
 
-    for x, y in genedictionary.items():
-        exonnumber = x
-        strand = y[0]
-        start = y[1]
-        end = y[2]
-
-        if strand == '+':
-            sequence = genome.seq[int(start):int(end)]
-            concatenate += sequence
-
-        else:
-            sequence = genome.seq[int(start):int(end)].reverse_complement()
-            concatenate += sequence
-
-print('>' + genus + ' ' + species + ' ' + gene)
-print(concatenate)
-print()
+out.close()
